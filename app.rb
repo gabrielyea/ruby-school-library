a8c95361d326b3f7d6ac9f8865e37c3c93dfa572
Gabriel <gabriel.gabrielyea@gmail.com>
2021-10-06 13:31:28 -0500

    Fix linter

--- app.rb
+++ app.rb
@@ -16,10 +16,17 @@ classroom1.add_student(student1)
 classroom2.add_student(student2)
 classroom2.add_student(student3)
 
+book1 = Book.new('book2', 'author 1')
+book2 = Book.new('book1', 'author 2')
+
+all_rentals = [Rental.new('march', student1, book1), Rental.new('june', student1, book2),
+               Rental.new('july', student2, book1)]
+
 puts ' '
 puts '--CLASS 1--'
 classroom1.students.each { |student| p student.name }
 puts '------'
+
 puts '--CLASS 2--'
 classroom2.students.each { |student| p student.name }
 puts '------'
@@ -27,12 +34,6 @@ puts '------'
 puts '--STUDENTS AND CLASSES--'
 all_students.each { |student| p("#{student.name} goes to #{student.classroom.label}") }
 puts '------'
-
-book1 = Book.new('book2', 'author 1')
-book2 = Book.new('book1', 'author 2')
-
-all_rentals = [Rental.new('march', student1, book1), Rental.new('june', student1, book2),
-               Rental.new('july', student2, book1)]
 
 puts '--RENTALS AND STUDENTS--'
 all_rentals.each do |rent|
