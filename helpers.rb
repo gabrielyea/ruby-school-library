b590b2baae90056ff11fe0e98ebb57242cc72fda
Gabriel <gabriel.gabrielyea@gmail.com>
2021-10-07 20:04:21 -0500

    Add Display_by_id lambda

--- helpers.rb
+++ helpers.rb
@@ -15,16 +15,20 @@ module Helpers
     return max
   end
 
-  List_Collection = lambda do |collection, title, labels, callback|
-    callback.call
+  List_Collection = lambda do |collection, title, labels, callback, *options|
+    callback.call(collection, title, labels, *options)
+  end
+
+  Nomral_Display = lambda do |collection, title, labels|
     max = max_string_length.call(collection, labels)
-    p max
     temp_string = ''
     labels.each { |label| temp_string += " #{label.to_s.capitalize.ljust(max + 2, '.')}" }
+
     puts "Total number of #{title}: #{collection.count}"
     puts
     puts temp_string
     puts
+
     collection.each do |obj|
       temp_string = ''
       labels.each { |label| temp_string += " #{convert_to_instance.call(label, obj).to_s.ljust(max + 2, '.')}" }
@@ -33,9 +37,15 @@ module Helpers
     puts
   end
 
-  Nomral_Display = lambda do |collection, title, labels| 
-    
-
+  Display_By_Id = lambda do |collection, title, labels, id|
+    result = collection.select { |rent| rent.person.id == id }
+    table = result.map do |res|
+      temp = Object.new
+      temp.instance_variable_set(:@date, res.date)
+      temp.instance_variable_set(:@name, res.person.name)
+      temp.instance_variable_set(:@title, res.book.title)
+      temp
+    end
+    Nomral_Display.call(table, title, labels)
   end
-  
 end
--- main.rb
+++ main.rb
@@ -3,10 +3,11 @@ require_relative 'book'
 require_relative 'student'
 require_relative 'classroom'
 require_relative 'teacher'
+require_relative 'rental'
 
 class App
   include Helpers
-  attr_accessor :all_options, :books, :people
+  attr_accessor :all_options, :books, :people, :rentals
 
   Option = Struct.new(:desc, :ref)
 
@@ -22,22 +23,27 @@ class App
     ]
 
     classroom1 = ClassRoom.new('101')
-    student = Student.new(classroom: classroom1, name: 'gabriel', age: '20')
+    student_a = Student.new(classroom: classroom1, name: 'gabriel', age: '20')
+    student_b = Student.new(classroom: classroom1, name: 'leo', age: '30')
     teacher = Teacher.new('Biology', name: 'Mr. Profesor', age: '50')
     book_a = Book.new(title: 'OOP and OOD with Ruby examples')
     book_b = Book.new(title: 'Twenty Thousand Leagues under the Sea', author: 'Julio Verne')
     book_c = Book.new(title: 'The Picture of Dorian Gray', author: 'Oscar Wilde')
+    rent_a = Rental.new(date: 'today', person: student_a, book: book_a)
+    rent_b = Rental.new(date: 'yesterday', person: student_b, book: book_b)
+    rent_c = Rental.new(date: 'tomorrow', person: student_a, book: book_c)
 
     @books = [book_a, book_b, book_c]
-    @people = [student, teacher]
+    @people = [student_a, teacher, student_b]
+    @rentals = [rent_a, rent_b, rent_c]
   end
 
   def list_all_books
-    List_Collection.call(books, 'Books', %i[title author type], Callback1)
+    List_Collection.call(books, 'Books', %i[title author], Nomral_Display)
   end
 
   def list_all_people
-    List_Collection.call(people, 'People', %i[id name age parent_permission])
+    List_Collection.call(people, 'People', %i[id name age], Nomral_Display)
   end
 
   def create_person
@@ -62,27 +68,45 @@ class App
   end
 
   def list_rentals_by_id
-    p 'list_rentals_by_id'
+    List_Collection.call(rentals, 'Rentals', %i[date name title], Display_By_Id, @people[0].id)
   end
 
   def exit
     p 'Good bye!'
   end
-end
 
-@app = App.new
+  def display_menu
+    puts 'Select an option by writing its number'
+    all_options.each_with_index do |op, index|
+      p "#{index + 1} #{op.desc}"
+    end
+  end
 
-def display_menu
-  puts 'Select an option by writing its number'
-  @app.all_options.each_with_index do |op, index|
-    p "#{index + 1} #{op.desc}"
+  def run
+    loop do
+      display_menu
+      user_input = gets.chomp.to_i
+      all_options[user_input - 1].ref.call
+
+      break if user_input == 7
+    end
   end
 end
 
-loop do
-  display_menu
-  user_input = gets.chomp.to_i
-  @app.all_options[user_input - 1].ref.call
+@app = App.new
+@app.run
 
-  break if user_input == 7
-end
+# def display_menu
+#   puts 'Select an option by writing its number'
+#   @app.all_options.each_with_index do |op, index|
+#     p "#{index + 1} #{op.desc}"
+#   end
+# end
+
+# loop do
+#   display_menu
+#   user_input = gets.chomp.to_i
+#   @app.all_options[user_input - 1].ref.call
+
+#   break if user_input == 7
+# end
