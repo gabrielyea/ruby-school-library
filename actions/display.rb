cc9971c406ecc9dd190eb78ec595c90158347992
Gabriel <gabriel.gabrielyea@gmail.com>
2021-10-11 16:35:10 -0500

    Define rentals by id

--- actions/display.rb
+++ actions/display.rb
@@ -1,7 +1,9 @@
 require_relative '../modules/display_module'
+require_relative '../modules/input_handler_module'
 
 class Display
   include DisplayModule
+  include InputHandler
   attr_accessor :data
 
   def initialize(data)
@@ -14,5 +16,8 @@ class Display
 
   def list_all_books
     List_Collection.call(data.books, 'Books', %i[title author], Display_Table)
+  end
+
+  def list_rentals_by_id
   end
 end
