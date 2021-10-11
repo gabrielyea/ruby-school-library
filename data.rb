4e300bffacd7c14d4693578e4e77c9b88409812a
Gabriel <gabriel.gabrielyea@gmail.com>
2021-10-11 18:17:15 -0500

    Change file locations

--- data.rb
+++
@@ -1,9 +0,0 @@
-class Data
-  attr_accessor :books, :people, :rentals
-
-  def initialize(books: [], people: [], rentals: [])
-    @books = books
-    @people = people
-    @rentals = rentals
-  end
-end
